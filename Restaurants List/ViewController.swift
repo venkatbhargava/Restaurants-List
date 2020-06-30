0ef0e0ded46ac1d2c8320b9a9bce9647429b81d1
Venkata Bhargav <bhargav.venkata.a@gmail.com>
2020-06-30 09:53:37 +0530

    Initial Commit

---
+++ Restaurants List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurants List
+//
+//  Created by Venkata Bhargav on 30/06/20.
+//  Copyright © 2020 venkat bhargav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
